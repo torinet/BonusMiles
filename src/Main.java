50ad62c0791ae45765367a74a87e7b2e5e397e4c
Vi <vikky96_96@mail.ru>
2023-09-22 00:58:16 +0300

    Gitignore

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 23_567;
+        int bonus = price / 20;
+        System.out.println("Бонусы: " + bonus);
+    }
+}
